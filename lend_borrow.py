823c41b78791b201a6dd8d8cf72825b1ae292334
hagankj <112912569+hagankj@users.noreply.github.com>
2023-03-16 18:58:44 -0500

    updated with design changes.

--- lend_borrow.py
+++ lend_borrow.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from dataclasses import dataclass
 import yfinance as yf
-#from crypto_wallet import generate_account, get_balance, send_transaction, access_treasury_account
 from web3 import Web3
 import time
 from dotenv import load_dotenv
@@ -12,16 +10,11 @@ import pandas as pd
 import Interest_rate as it_rate
 from notification_manager import send_notification
 
+
 ######################################################
-###### Define Variables and Load Smart Contract ######
-######################################################
+################ Load Smart Contract #################
 
 load_dotenv()
-
-# twilio_number = os.getenv("VIRTUAL_TWILIO_NUMBER")
-# user_number = os.getenv("VERIFIED_NUMBER")
-# twilio_sid = os.getenv("TWILIO_SID")
-# twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
 # creates a Web3 instance of the Ganache network on local machine
 w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
@@ -29,7 +22,7 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 st.set_page_config(layout='wide')
 
 
- #Smart Contract connection
+# Smart Contract connection and store in in Streamlit cache
 @st.cache(allow_output_mutation=True)
 def load_contract():
     ### UPDATE CONTRACT ABI INFO AFTER DEPLOYMENT ###
@@ -50,25 +43,9 @@ def load_contract():
 # Initaliize contract
 contract = load_contract()
 
-# Load user account
-user_account = w3.eth.accounts[0]
 
-# Load user account balance
-user_balance = w3.eth.get_balance(w3.eth.accounts[0])/10**18
-
-# Load contract(treasury) address
-treasury_address = os.getenv('SMART_CONTRACT_ADDRESS')
-
-# Load contract(treasury) balance
-treasury_balance = w3.eth.get_balance(os.getenv('SMART_CONTRACT_ADDRESS'))/10**18
-
-# Define User Borrow Balance
-borrow_balance = contract.functions.borrowBalance(user_account).call()
-
-# Define User Lend Balance
-lend_balance = contract.functions.lendBalance().call()
-
-# Define intrest rate variables
+######################################################
+########## Define intrest rate variables #############
 
 # Define default util_rate
 util_rate = .5
@@ -88,51 +65,64 @@ slope2 = 1.5
 # Error Flag if the Util rate is greater than 1
 over_borrow = False
 
-######################################################
+lend_interest_rate = base_rate + slope1/2
+borrow_interest_rate = base_rate +slope1
+
 ######################################################
 ############ Solidity Contract Functions #############
 
+# Function to interact with solidity contract
 def solidity_function(func, amount=None):
+    user_account = w3.eth.accounts[0]
+    # Function for lending
     if func =='lend':
         lend_amount = amount
         lend_amount_wei = Web3.toWei(lend_amount*10**18, 'wei')
         lend = contract.functions.lend().transact({'value': lend_amount_wei,'from': w3.eth.accounts[0]})
         lend_balance = contract.functions.lendBalance().call()
         return lend_balance
+    # Function for borrowing
     elif func =='borrow':
         borrow_amount = amount
         borrow_amount_wei = Web3.toWei(borrow_amount*10**18, 'wei')
         borrow = contract.functions.borrow(borrow_amount_wei).transact({'from': w3.eth.accounts[0]})
         borrow_balance = contract.functions.borrowBalance(user_account).call()
         return borrow_balance
+    # Function to repay borrow_balance
     elif func =='repay':
         repay_amount = amount
         repay_amount_wei = Web3.toWei(repay_amount*10**18, 'wei')
         repay = contract.functions.repay(repay_amount_wei).transact({'value': repay_amount_wei, 'from': w3.eth.accounts[0]})
         new_borrow_balance = contract.functions.borrowBalance(user_account).call()
         return new_borrow_balance
+    # Function to withdraw lend_balance
     elif func == 'withdraw':
         withdraw_amount = amount
         withdraw_amount_wei = Web3.toWei(withdraw_amount*10**18, 'wei')
         withdraw = contract.functions.withdraw(withdraw_amount_wei).transact({'from': w3.eth.accounts[0]})
         withdraw_balance = contract.functions.borrowBalance(user_account).call()
         return withdraw_balance
+    # Check user_balance
     elif func == 'user_balance':
         return w3.eth.get_balance(w3.eth.accounts[0])/10**18
+    # Check borrow_balance
     elif func == 'borrow_balance':
         return contract.functions.borrowBalance(w3.eth.accounts[0]).call()/10**18
+    # Check lend_balance
     elif func == 'lend_balance':
         return contract.functions.lendBalance().call()/10**18
+    # Check treasury_balance
     elif func == 'treasury_balance':
         return w3.eth.get_balance(os.getenv('SMART_CONTRACT_ADDRESS'))/10**18
     
-######################################################    
 ######################################################
-######################################################
+############### Main Streamlit Application ###########
 
-st.title(':green[------------------------------------]:blue[$PyBo$]:green[Lend]:blue[------------------------------------]')
+# Create title for streamlit app
+st.markdown("<h1 style='text-align: center;'><FONT COLOR=blue><i>Py</i><FONT COLOR=green>Bo<FONT COLOR=blue>Lend</h1>",unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'><FONT COLOR=green>------------------------------<FONT COLOR=blue>------------------------------</h1>",unsafe_allow_html=True)
 
-
+# Pulls data using the yfinance API to determine liquidation risk based on price movement.
 eth_price = yf.download(tickers='ETH-USD',period='1d', interval='5m',rounding=True).drop(columns=['Adj Close','Open','High','Low','Volume'])
 eth_price['Percent Change']=eth_price['Close'].pct_change()
 percent_change=eth_price['Percent Change'].mean()*100
@@ -140,105 +130,74 @@ percent_change=eth_price['Percent Change'].mean()*100
 # Initialize column interface for streamlit
 treasury_col, functions_col, account_col = st.columns([1,4.75,1])
 
-# Column that displays treasury data
-with treasury_col:
-    st.header('Treasury Balance') 
-    st.write(solidity_function('treasury_balance'), 'ETH')
-    # liquidate signal
-    if percent_change >= 1:
-        #st.write(f'Average Price Change :red[{percent_change:.3f}]%')
-        st.header('Liquidate Risk')
-        st.write(':red[High]')
-    elif percent_change <= 1:
-        #st.write(f'Average Price Change :green[{percent_change:.3f}]%')
-        st.header('Liquidate Risk')
-        st.write(':green[Low]')
-
-
-# Column that displays user account balance
-with account_col:
-# Loads account credentials
-    if user_balance != None:
-        st.header('Account Status')
-        st.write(':green[Connected]')
-        st.header('Account Balance')
-        st.write(solidity_function('user_balance'), 'ETH')
-    else:
-        st.write('Please ensure your Mneumonic phrase is saved in a .env in this same directory.')
-        st.write('Then restart this application.')
+###
+## treasury_col and account_col at bottom of file so the balances updates after actions
+###
 
 # Column that contains the functions to interact with smart contract
 with functions_col:
     
     st.subheader('The :violet[Premier] ETH Lending and Borrowing Application')
 
-    # Create the Tabs for streamlit
+    # Create the tabs for streamlit
     lend_tab,borrow_tab,repay_tab,withdraw_tab,balances_tab,time_tab = st.tabs(['Lend', 'Borrow', 'Repay', 'Withdraw', 'Balances', 'Time Advance'])
 
-    # Creates Lend Tab
+    # Creates Lend tab
     with lend_tab:
 
-        st.header('Lend')
-        st.write('Starting Balance:', solidity_function('user_balance'), 'ETH')    
+        st.header('Lend')   
 
         lend_amount = st.number_input('Enter the amount you want to lend (in ETH):')
+
+        notification_number = st.text_input('Enter your phone number to receive confirmation via text message:')
+        
         if lend_amount != 0:
-            lend_interest_rate = st.write(f'{(lend_amount/treasury_balance * .5):.2}% Lending Interest' )
+            st.write(f'{lend_interest_rate:.2}% Lending Interest' )
         else:
-            lend_interest_rate = 0
+            lend_interest_rate
 
         
         if st.button('Complete Lend',key='lend'):
 
-            # sending loan to the TREASURY_ADDRESS
+            # Sending loan to the TREASURY_ADDRESS
             solidity_function('lend', lend_amount)
-            #lend_amount_wei = Web3.toWei(lend_amount*10**18, 'wei')
-            #lend_activity = contract.functions.lend().transact({'value': lend_amount_wei, 'from': w3.eth.accounts[0]})
 
-            balance = user_balance
             st.write(f'{lend_amount} has been deducted from your personal wallet.')    
-            st.write(f'We owe you {lend_amount} + {(lend_amount/treasury_balance * .5):.2}% interest.')   
-            st.write('New Balance:', solidity_function('user_balance'))
+            st.write(f'We owe you {lend_amount} + {lend_interest_rate}% interest.')   
 
             # updates the balance of the TREASURY_ADDRESS
             solidity_function('treasury_address')
-            #updated_treasury_balance = w3.eth.get_balance(os.getenv('SMART_CONTRACT_ADDRESS'))/10**18
-            st.write(f'The new treasury balance is:' , solidity_function('treasury_balance'), 'ETH')
+
+            # Sends SMS notification 
+            if notification_number != None:
+                try:
+                    send_notification(f"Transaction confirmed. You have received your deposit of {lend_amount}ETH. The amount you may borrow has increased by {lend_amount * 0.8}ETH.", f"+1{notification_number}") 
+                except:
+                    st.write("The number you entered may not be valid, but the above information confirms your transaction.")
 
 
-            updated_treasury_balance = w3.eth.get_balance(os.getenv('SMART_CONTRACT_ADDRESS'))/10**18
-            st.write(f'The new treasury balance is: {updated_treasury_balance} ETH')
-
-
-            # Sends SMS notification - NOTE the line below works, but commenting out until closer to presentation to limit trial uses
-            # send_notification(f"Transaction confirmed. You have received your deposit of {lend_amount}ETH. The amount you may borrow has increased by {lend_amount * 0.8}ETH.") 
-
-
-    # Creates Borrow Tab
+    # Creates Borrow tab
     with borrow_tab:
         st.header('Borrow')
-        balance = user_balance
         borrow_amount = st.number_input('Enter the amount you want to borrow (in ETH):')
         if borrow_amount != 0:
-            borrow_interest_rate = st.write(f'{(borrow_amount/treasury_balance * 2):.2}% Borrow Interest')
+            st.write(f'{(borrow_interest_rate):.2}% Borrow Interest')
         else:
             # Choose and insert default value for borrow_interest_rate
-            borrow_interest_rate = 0
+            borrow_interest_rate
             
 
         if st.button('Complete Borrow',key='borrow'):
 
             # sending loan to the TREASURY_ADDRESS
-            #send_transaction(w3=w3, account=TREASURY_ACCOUNT_OBJECT, to=user_account.address, amount=borrow_amount)
             solidity_function('borrow', borrow_amount)
             #balance = user_balance
             st.write(f'{borrow_amount} ETH has been sent to your personal wallet.')    
-            st.write(f'You owe us {borrow_amount} + {(borrow_amount/treasury_balance * 2):.2}% interest.')    
+            st.write(f'You owe us {borrow_amount} + {(borrow_interest_rate * 2):.2}% interest.')    
             st.write('New Borrow Balance:', solidity_function('borrow_balance'))    
             # updates the balance of the TREASURY_ADDRESS
             solidity_function('treasury_address')
-            st.write(f'The new treasury balance is:' , solidity_function('treasury_balance'), 'ETH')
-
+           
     # Creates Repay Tab
     with repay_tab:
         st.header('Repay')
@@ -252,8 +211,9 @@ with functions_col:
     
     with withdraw_tab:
         st.header('Withdraw')
-        st.write(f'All Borrows must be paid before withdraw. Current Borrow Balance:', solidity_function('borrow_balance'),'ETH')
-        
+        st.subheader('All Borrows must be paid before withdraw.')
+        st.write('Current Borrow Balance:', solidity_function('borrow_balance'),'ETH')
+        st.write('Current Lend Balacne:', solidity_function('lend_balance'),'ETH')
         withdraw_amount = st.number_input('Amount to Withdraw')
         if st.button('Submit'):
             solidity_function('withdraw', withdraw_amount)
@@ -263,7 +223,6 @@ with functions_col:
     with balances_tab:
         st.header('Balances')
         if st.button('Get Balances'):
-            balance = user_balance
             st.write(f' Account Balance:', solidity_function('user_balance'), 'ETH')
             st.write(f'Borrow Balance:', solidity_function('borrow_balance'), 'ETH')
             st.write(f'Lend Balance:', solidity_function('lend_balance'), 'ETH')
@@ -280,9 +239,57 @@ with functions_col:
 
         increment = st.button('Advance Time')
         if increment:
+
             st.session_state.count += 1
 
+
+            #calculate utlization rate
+            over_borrow, util_rate = it_rate.utilization_rate(solidity_function('treasury_balance'),solidity_function('borrow_balance'))
+
+            #set borrow interest rate
+            borrow_interest_rate = it_rate.interest_rate(util_rate, util_optimal, base_rate, slope1, slope2, over_borrow)
+
+
+            #set lend interest rate
+            lend_interest_rate = borrow_interest_rate/2
+
+
+
+            borrow_interest_amount = it_rate.interest_to_pay(borrow_interest_rate, solidity_function('borrow_balance'), 0)
+            
+
+            solidity_function('lend',borrow_interest_amount/2)
+            solidity_function('repay',borrow_interest_amount/2)
+            solidity_function('borrow',borrow_interest_amount)
+
+
+
         st.write('Interest Time', st.session_state.count)
+        st.write('Lend Intrest Rate',lend_interest_rate )
+    
 
+# Column that displays treasury data
+with treasury_col:
+    st.header('Treasury Balance') 
+    st.write(solidity_function('treasury_balance'), 'ETH')
+    # liquidate signal
+    if percent_change >= 1:
+        #st.write(f'Average Price Change :red[{percent_change:.3f}]%')
+        st.header('Liquidate Risk')
+        st.write(':red[High]')
+    elif percent_change <= 1:
+        #st.write(f'Average Price Change :green[{percent_change:.3f}]%')
+        st.header('Liquidate Risk')
+        st.write(':green[Low]')
 
-
+# Column that displays user account balance
+with account_col:
+# Loads account credentials
+    if solidity_function('user_balance') != None:
+        st.header('Account Status')
+        st.write(':green[Connected]')
+        st.header('Account Balance')
+        st.write(solidity_function('user_balance'), 'ETH')
+    else:
+        st.write('Please ensure your Mneumonic phrase is saved in a .env in this same directory.')
+        st.write('Then restart this application.')
